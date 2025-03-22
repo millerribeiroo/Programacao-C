fa4ab7245a11e8eaf41255147c6336c7574d6fc1
millerribeiroo <millerrib@gmail.com>
2025-03-22 15:47:26 +0000

    teste de operadores matematicos simples

--- OperadoresMatematicos.c
+++ OperadoresMatematicos.c
@@ -1,9 +1,39 @@
-
+// treinando com operadores matematicos
 
 #include <stdio.h>
 
 int main(){
+    //Declarando variaveis
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
 
+    //Soma (+)
+    //Subtração (-)
+    //Multiplicação (*)
+    //Divisão (/)
+    
+    //operação soma
+    soma = numero1 + numero2;
+
+    //operação subtração
+    subtracao = numero1 - numero2;
+ 
+    //operação multiplicação
+    multiplicacao = numero1 * numero2;
+
+    //operação divisão 
+    divisao = numero1 / numero2;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &numero1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%d", &numero2);
+
+    printf("A soma dos números é: %d\n", soma);
+    printf("A subtração dos números é: %d\n", subtracao);
+    printf("A multiplicação dos números é: %d\n", multiplicacao);
+    printf("A divisão dos números é: %d\n", divisao);
 
 
 
