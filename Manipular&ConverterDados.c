a46af059e32b97b888df6f77240902a90691649a
millerribeiroo <millerrib@gmail.com>
2025-03-22 16:33:36 +0000

    inicio da aula de Manipulação de Variaveis

---
+++ Manipular&ConverterDados.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    int main(){
+        int a = 10;
+        int b = 3;
+        //conversão de variáveis
+        //existe 2 tipos de conversões de variáveis:
+        //conversão implícita e conversão explícita
+        //conversão implícita é quando o compilador faz a conversão automaticamente
+        //conversão explícita é quando o programador faz a conversão
+        float c = a / b; //conversão implícita
+        printf("c = %f\n", c);
+
+        float d = (float) a / b; //conversão explícita
+        printf("d = %f\n", d);
+
+        return 0;
+    }
