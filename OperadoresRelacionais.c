8297259ecd2456126a2bf67df8c78d50c801256b
millerribeiroo <millerrib@gmail.com>
2025-03-22 22:28:25 +0000

    Aula finalizada de OperadoresRelacionais

---
+++ OperadoresRelacionais.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    //Operadores Relacionais
+    // > Maior que;
+    // < Menor que;
+    // >= Maior ou igual a;
+    // <= Menor ou igual a;
+    // == Igual a;
+    // != Diferente de;
+
+    float a = 10.2;
+    int b = 10;
+
+    printf("a > b: %d\n", (int)a > b);
+    printf("a <= b: %d\n", a <= b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n\n", a != b);
+
+    return 0;
+}
