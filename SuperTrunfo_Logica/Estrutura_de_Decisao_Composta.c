c8bfad70e8bc8dd24a0598e2387403d9e82d6026
millerribeiroo <millerrib@gmail.com>
2025-03-24 17:54:29 +0000

    Organização em pastas e conclusão da aula de Estrutura_de_Decisao_Composta

---
+++ SuperTrunfo_Logica/Estrutura_de_Decisao_Composta.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+    int main() {
+        int numero = 11;
+       
+        
+        if (numero % 2 == 0) {
+            //bloco de código caso a condição seja verdadeira
+            printf("O número é Par.\n\n\n");
+        } else {
+            //bloco de código caso a condição seja falsa
+            printf("O número é impar\n\n\n");
+        }
+
+        return 0;
+    }
