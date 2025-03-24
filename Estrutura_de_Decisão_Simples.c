89cd59155f0958a00ac9be4e71eadcd0dda6bbfa
millerribeiroo <millerrib@gmail.com>
2025-03-24 17:42:51 +0000

    Aula concluída

---
+++ Estrutura_de_Decisão_Simples.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    int main() {
+        int idade;
+
+        // se a 'idade' for maior que 18, então imprime a mensagem
+        idade = 20;
+       
+
+        if (idade >= 18){
+            //bloco de código a ser executado, caso nao tem '{}' apenas a proxima linha é executada.
+            printf("voce é maior de idade\n");
+        }
+        
+        
+        
+        return 0;
+    }
