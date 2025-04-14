a7f870d8dfb30a9c03398ecb25bfcc6ddccb7335
millerribeiroo <millerrib@gmail.com>
2025-04-14 15:55:35 +0000

    Aula concluída

---
+++ Declarar_Loops_Avancado.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Loops avançados:
+    for (i = 0, j = 10; i < j; i++, j--) {
+        printf("i: %d, j: %d\n", i, j);
+    }
+
+    return 0;
+}
